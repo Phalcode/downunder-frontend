0876106e9072f2b8870cc41b48043ec7bc32a770
Alper Alkan <22225051+Alfagun74@users.noreply.github.com>
2021-01-06 15:13:20 +0100

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { StartComponent } from "./components/start/start.component";
 
-const routes: Routes = [];
+// eslint-disable-next-line prettier/prettier
+const routes: Routes = [
+  { path: "", pathMatch: "full", component: StartComponent },
+  { path: "**", redirectTo: "" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
