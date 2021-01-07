02054bc6d0bdcf6f44a21ddd07e88150cfa8c2fc
Alper Alkan <22225051+Alfagun74@users.noreply.github.com>
2021-01-07 04:30:05 +0100

    pwa impl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,8 @@ import { environment } from "../environments/environment";
     AppRoutingModule,
     HttpClientModule,
     ServiceWorkerModule.register("ngsw-worker.js", {
-      enabled: environment.production
+      enabled: environment.production,
+      registrationStrategy: "registerImmediately"
     })
   ],
   providers: [],
